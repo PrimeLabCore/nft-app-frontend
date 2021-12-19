12f087b68dcc9c3e892ac4249fdf528854699758
mikeptmorton <mikeptmorton@gmail.com>
2021-12-18 19:08:14 -0500

    Sets up Google Analytics tracking for G-VHQDFLX5Y2, defines adroll properties before using them

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import ReactGA from 'react-ga';
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import store from "./store";
+
+ReactGA.initialize('G-VHQDFLX5Y2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
