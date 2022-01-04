ed141cf3aa2b62d831262232124ba59354b9fa3c
Biswanath sahoo <biswanath774@gmail.com>
2022-01-05 02:57:17 +0530

    removing info icon from signup screen

--- src/Components/SignUp/CreateAnAccount/CreateAnAccount.jsx
+++ src/Components/SignUp/CreateAnAccount/CreateAnAccount.jsx
@@ -231,9 +231,6 @@ const CreateAnAccount = () => {
 
         {/* input field for full name */}
         <div className={styles.textField}>
-          {info === "name" ? (
-            <BsInfoCircleFill className={styles.infoIcon} />
-          ) : null}
           <TextFieldComponent
             label="FULL NAME"
             variant="outlined"
@@ -248,9 +245,6 @@ const CreateAnAccount = () => {
 
         {/* input field for account id */}
         <div className={styles.textField}>
-          {info === "id" ? (
-            <BsInfoCircleFill className={styles.infoIcon} />
-          ) : null}
           <TextFieldComponent
             label="ACCOUNT ID"
             variant="outlined"
