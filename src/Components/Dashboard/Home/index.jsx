7e7e1fdd107f75d66428b44c4f84aeee878efebe
Biswanath sahoo <biswanath774@gmail.com>
2022-01-06 03:37:25 +0530

    Import contact screen issue

--- src/Components/Dashboard/Home/index.jsx
+++ src/Components/Dashboard/Home/index.jsx
@@ -13,10 +13,12 @@ import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import ImportContactsDialog from "../../ImportContactsDialog/ImportContactsDialog";
 import {useNavigate} from "react-router-dom";
+import ContactPopup from "../../../common/components/ContactPopup";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const [importContactDialog, setimportContactDialog] = useState(false);
+  const [importContactDialog, setImportContactDialog] = useState(false);
+  const [showContactListPopup, setShowContactListPopup] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem("welcome") === "true") {
@@ -27,36 +29,41 @@ const Home = () => {
   let navigate = useNavigate();
 
   const HandleDialogOpen = () => {
-    setimportContactDialog(true);
+    setImportContactDialog(true);
   };
 
   const HandleDialogClose = () => {
-    setimportContactDialog(false);
+    setImportContactDialog(false);
   };
 
   const importContact = (data) => {
     if (data) {
+      setImportContactDialog(false);
+      setShowContactListPopup(true);
       dispatch({
         type: "getGoogleContactData",
         payload: data,
       });
-      setimportContactDialog(false);
     }
   };
 
   const contactImportCallback = (error, source) => {
-    HandleDialogClose();
-
+    setImportContactDialog(false);
     if (error) {
       toast.error(`Something Went Wrong Fetching Contacts From ${source}`);
       dispatch({ type: "createnft__open" });
       return;
     } else {
-      dispatch({ type: "createnft__open" });
+      // dispatch({ type: "createnft__open" });
       toast.success(`Your Contacts Were Successfully Imported From ${source}`);
       return;
     }
   };
+
+  const openCreateNFTPopup = ()=>{
+    setShowContactListPopup(true);
+    dispatch({ type: "createnft__open" });
+  }
 
   return (
     <>
@@ -69,6 +76,21 @@ const Home = () => {
             onImport={importContact}
             status={importContactDialog}
             callback={contactImportCallback}
+          />
+
+          <ContactPopup
+            show={showContactListPopup}
+            onClose={()=>{
+              openCreateNFTPopup()
+            }}
+            onBack={()=>{
+              openCreateNFTPopup
+            }}
+            title={"Gift an NFT"}
+            btnText={"Send Gift"}
+            handleBtnClick={()=>{
+              openCreateNFTPopup()
+            }}
           />
 
           {/* Home Create NFT Container */}
