e0f1e8308bc68e9f3d0dab126cee04030296f9f2
Himalaya vats <rohitreactdev@gmail.com>
2022-01-08 11:38:44 +0530

    full name validation modified

--- src/Components/SignUp/CreateAnAccount/CreateAnAccount.jsx
+++ src/Components/SignUp/CreateAnAccount/CreateAnAccount.jsx
@@ -121,7 +121,7 @@ const CreateAnAccount = () => {
 
     //signup body
     let user = {
-      fullName: fullname,
+      fullName: fullname.trim(),
       walletName: accountId.includes(".near") ? accountId : accountId + ".near",
       email: signupEmail,
       phone: signupPhone,
@@ -151,9 +151,16 @@ const CreateAnAccount = () => {
       });
   };
 
+  const isFullNameValid = (fullname) => {
+    var format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return format.test(fullname);
+  }
+
   const isFormValid = () => {
     let returnVal = true;
     if (fullname == "") {
+      returnVal = false;
+    } else if (isFullNameValid(fullname)) {
       returnVal = false;
     } else if (
       accountId == "" ||
@@ -233,16 +240,15 @@ const CreateAnAccount = () => {
               ),
             }}
             HandleFocus={() => HandleFocus("id")}
-            // disabled
+          // disabled
           />
         </div>
 
         {/* create account button */}
         <button
           onClick={handleSignup} // createAccount
-          className={`${styles.secondary_button} ${
-            isFormValid() ? styles.active_button : ""
-          }`}
+          className={`${styles.secondary_button} ${isFormValid() ? styles.active_button : ""
+            }`}
           disabled={!isFormValid()}
         >
           Create an account
