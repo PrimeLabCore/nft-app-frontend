b106e9996e610cfb3a9244bcaf789d5da328d31a
Doug Warner <idougwarner@gmail.com>
2022-02-11 16:16:09 -0800

    remove ga

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import ReactGA from 'react-ga';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { persistor, store } from "./store";
-
-ReactGA.initialize('G-4N8P08L7XC');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <BrowserRouter>
